6df8de69f427eb12e3e674cd1ec6bd1f674f1a82
alberto lotz <alberto10lotz@gmail.com>
2019-10-30 23:38:31 -0300

    created upload avatar em relation db

---
+++ src/app/controllers/fileController.js
@@ -0,0 +1,16 @@
+import File from '../models/file';
+
+class FileController {
+  async store(req, res) {
+    const { originalname: nome, filename: path } = req.file;
+
+    const file = await File.create({
+      nome,
+      path,
+    });
+
+    return res.json(file);
+  }
+}
+
+export default new FileController();
