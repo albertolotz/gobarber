849c6974c7eeea65d6a5487899bf8bad63d332ed
alberto lotz <alberto10lotz@gmail.com>
2019-11-01 22:53:14 -0300

    created mongo db and config if

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,93 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import User from '../models/User';
+import File from '../models/file';
+import Appointment from '../models/appointment';
+
+class AppointmentController {
+  // metodo index = listgem
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'nome'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+    return res.json(appointments);
+  }
+
+  // metodo store = inclui
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'dados inválidos!' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    // check se provider_id é um provider
+
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Agendamento apenas possivl com prestador!' });
+    }
+
+    // verificação de data informada não é anterios a atual.
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Data Passada não é permitida!' });
+    }
+
+    // verifica se já existe alguma agenda para o horário
+    const checkAvalability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvalability) {
+      return res
+        .status(400)
+        .json({ error: 'Já existe agendamento para este horário!' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date: hourStart,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import multer from 'multer';
-import multerConfig from './config/multer';
-
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
+import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/fileController';
 import ProviderController from './app/controllers/providerController';
-
+import scheduleController from './app/controllers/scheduleController';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,6 +19,9 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/schedule', scheduleController.index);
 
 export default routes;
